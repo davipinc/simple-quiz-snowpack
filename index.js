ac2b311657554a5d271e9ac0052bbf6542ae4fb1
David Pinching <dpinching@yahoo.co.uk>
2020-12-29 18:50:37 +0000

    Basic quiz with LitHTML and SnowPack

---
+++ index.js
@@ -0,0 +1,4 @@
+// my-first-snowpack/index.js
+import { quiz } from './quiz.js';
+
+quiz();
---
+++ quiz.js
@@ -0,0 +1,89 @@
+import {html, render} from 'lit-html';
+
+const app = document.querySelectorAll('#app')[0];
+const initialState = {
+  currentQuestion: 0
+};
+
+const state = {};
+
+const questions = [
+  {
+    instruction: html`<h2>foo</h2><p>bar</p>`
+  },
+  {
+    instruction: html`<h2>bat</h2><p>baz</p>`
+  }
+];
+// const sayHello = (name) => {
+  
+//   return html`<h1>Hello ${name}</h1>`;
+// };
+
+function renderQuestion() {
+  const { currentQuestion: currentQuestion } = state;
+  render(quizTemplate(questions[currentQuestion]), app);  
+}
+function nextQuestion() {
+  state.currentQuestion += 1;
+  renderQuestion();
+}
+
+function checkState(currentState = {}) {
+  const stateKeys = Object.keys(currentState);
+  const initialStateKeys = Object.keys(initialState);
+
+  const uninitialisedKeys = stateKeys.filter(key => initialState[key] === undefined);  
+  const removedKeys = initialStateKeys.filter(key => state[key] === undefined);  
+
+  if (uninitialisedKeys.length) {
+    console.warn('You have added state but not defined an initial value', uninitialisedKeys);
+  }
+  
+  if (removedKeys.length) {
+    console.warn('You have removed state items', removedKeys);
+  }
+
+  return {
+    uninitialisedKeys,
+    removedKeys
+  };
+}
+
+function removeKeys(uninitialisedKeys) {
+  uninitialisedKeys.forEach(key => {
+    console.info(`Removing state '${key}': ${state[key]}`);
+    delete state[key];
+  });
+}
+
+function initialiseState() {
+  Object.keys(initialState).forEach(key => {
+    state[key] = initialState[key];
+  });
+}
+
+function resetState() {
+  const { uninitialisedKeys } = checkState(state);
+  removeKeys(uninitialisedKeys);
+  initialiseState();
+  writePage();
+}
+
+const quizTemplate = (question) => html`
+  <section class="question" aria-live="polite">
+    <h1>Question ${state.currentQuestion + 1}</h1>
+    <p>${question.instruction}</p>
+  </section>
+  <button @click=${nextQuestion}>Next Question</button>
+  <button @click=${resetState}>Reset</button>
+`;
+
+function writePage() {
+  renderQuestion();
+}
+
+export function quiz() {
+  initialiseState();
+  writePage();
+}
